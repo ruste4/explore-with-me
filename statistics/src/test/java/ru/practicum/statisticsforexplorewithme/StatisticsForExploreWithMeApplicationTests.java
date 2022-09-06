390fe23fd937ecc59e0aec72224a8661d84ca076
ruste4 <te4rus@yandex.ru>
2022-09-06 10:43:27 +0500

    first commit

---
+++ statistics/src/test/java/ru/practicum/statisticsforexplorewithme/StatisticsForExploreWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.practicum.statisticsforexplorewithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticsForExploreWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
